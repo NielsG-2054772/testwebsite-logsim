92d0c8be04ca1d5c65e8664abea52b239ca22664
NielsG_2054772 <niels.gielen@student.uhasselt.be>
2022-06-04 16:26:55 +0200

    listen to correct port

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 app.use(express.static('dist'));
 
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+app.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
 });
